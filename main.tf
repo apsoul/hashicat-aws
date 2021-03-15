275bc2e718c506f2e0a75efcb25fd125789c8662
Alex Soul <alexander.soul@computacenter.com>
2021-03-15 11:16:13 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -126,6 +126,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Department = "devops" # PolicySet wasn't applied to workspace, re-running
+    Billable = true
   }
 }
 
